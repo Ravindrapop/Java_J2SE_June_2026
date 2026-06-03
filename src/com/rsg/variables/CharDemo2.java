5b1bd52449cb65fbfe5abcc02464f8351f280247
Ravindra Singh <ravindra12329@gmail.com>
2026-06-03 06:40:44 +0530

    code added for char data type poc

---
+++ src/com/rsg/variables/CharDemo2.java
@@ -0,0 +1,17 @@
+package com.rsg.variables;
+
+public class CharDemo2 {
+
+	public static void main(String[] args) {
+		
+		int i = 'c';
+		
+		char i2 = 97;
+		
+		
+		System.out.println(i);
+		System.out.println(i2);
+
+	}
+
+}
