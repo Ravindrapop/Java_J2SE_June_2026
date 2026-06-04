e2024a218a7e1313b3c6c2c23ad189c954e204aa
adityaahamana18 <adityaahamana18@gmail.com>
2026-06-04 06:54:20 +0530

    Code has been added for int demo

---
+++ src/com/aa/variables/IntDemo1.java
@@ -0,0 +1,12 @@
+package com.aa.variables;
+
+public class IntDemo1 {
+	public static void main (String[] args) {
+		int a = 10;
+		int b = 15;
+		int sum = a+b;
+		System.out.println("The Sum of a and b is: "+sum);
+		
+	}
+
+}
