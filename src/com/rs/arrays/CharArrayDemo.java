7c975d0b88ef8dfdb4c1478464cf659b29e244d5
unknown <ravindra12329@gmail.com>
2026-06-09 19:38:18 +0530

    Array example

---
+++ src/com/rs/arrays/CharArrayDemo.java
@@ -0,0 +1,18 @@
+package com.rs.arrays;
+
+public class CharArrayDemo {
+	
+	public static void main(String[]args) {
+		
+		char[] c = {'a','e','i','o','u'};
+		
+		System.out.println(c[4]);
+		
+		System.out.println(c.length);
+		
+		
+
+		
+	}
+
+}
